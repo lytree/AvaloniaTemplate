b5628065f741ef65f14e29344caf7735dd96dba9
lytree <hi.yangzh@outlook.com>
2026-05-05 21:41:52 +0800

    del semi

--- src/Avalonia.UI/Theme/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using Avalonia.Metadata;
-
-[assembly:XmlnsPrefix("https://irihi.tech/ursa/themes/semi", "u-semi")]
-[assembly:XmlnsPrefix("https://irihi.tech/semi", "semi")]
-[assembly:XmlnsDefinition("https://irihi.tech/semi", "Ursa.Themes.Semi")]
-[assembly:XmlnsDefinition("https://irihi.tech/ursa/themes/semi", "Ursa.Themes.Semi.Legacy")]
